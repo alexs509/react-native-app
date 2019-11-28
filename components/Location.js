088db0c76c7969f8862860d63182df682bd8691a
Alex <alex.rigueur@gmail.com>
2019-11-28 12:28:16 +0100

    update position

--- components/Location.js
+++ components/Location.js
@@ -1,85 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import MapView, { AnimatedRegion, Animated, Marker  } from 'react-native-maps';
-import { getDistance } from 'geolib';
-
+import { StyleSheet, Text, View, Button, ActivityIndicator } from 'react-native';
+import MapView, { AnimatedRegion, Animated, Marker } from 'react-native-maps';
 
 export default class Location extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {location:null, velib: null, all:[]};
-    var options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 30000
-    };
+    this.state = { location: null, all: null };
 
-    _getDistance = (lat1, long1, lat2, long2,) => {
-      var dis = getDistance(
-        { latitude: lat1, longitude: long1 },
-        { latitude: lat2, longitude: long2 }
-      );
-      return dis/1000;
-    };
-
-    console.log(_getDistance(48.914822, 2.381170, 48.929230, 2.386580));
-
-    let velibs = [];
     navigator.geolocation.getCurrentPosition(
       position => {
         const location = position;
         this.setState({ location });
+        return fetch('https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&rows=2&facet=overflowactivation&facet=creditcard&facet=kioskstate&facet=station_state&geofilter.distance=+' + this.state.location.coords.latitude + '%2C' + this.state.location.coords.longitude + '%2C5000')
+          .then((response) => response.json())
+          .then((responseJson) => {
+            this.setState({
+              all: responseJson.records,
+            }, function () {
+            });
+          })
+          .catch((error) => {
+            console.error(error);
+          });
       },
       error => Alert.alert(error.message),
       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
     );
   }
 
-  componentDidMount() {
-    return fetch('https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&rows=20')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        this.setState({
-          velib: responseJson.records,
-        }, function () {
-        });
-        (responseJson.records).map((resp) => {
-          let lat = resp.fields.geo[0];
-          let lng = resp.fields.geo[1];
-          if(resp.fields.geo){
-          
-          }else {
-
-          }
-          //this.state.all = [...this.state.all, resp.fields.geo]
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-
 
   render() {
+
     let lat = 48.865983;
     let long = 2.275725;
     let coord = {
-      latitude:null,
-      longitude:null
+      latitude: null,
+      longitude: null
     }
     let coord2 = {
-      latitude:48.865983,
-      longitude:2.275725
-      
+      latitude: 48.865983,
+      longitude: 2.275725
+
     }
-    if(this.state.all.length>0) {
-      console.log(this.state.all);
-    }
-    if(this.state.location!= null){ 
+    if (this.state.location != null) {
       console.log("state");
       coord.latitude = this.state.location.coords.latitude;
       coord.longitude = this.state.location.coords.longitude;
+      /* return fetch('https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&rows=2&facet=overflowactivation&facet=creditcard&facet=kioskstate&facet=station_state&geofilter.distance=+'+this.state.location.coords.latitude+'%2C'+this.state.location.coords.longitude+'%2C5000')
+          .then((response) => response.json())
+          .then((responseJson) => {
+            console.log(responseJson);
+            this.setState({
+              all: responseJson.records,
+            }, function () {
+            });
+          })
+          .catch((error) => {
+            console.error(error);
+          }); */
     }
+
     return (
       <View style={styles.container}>
 
@@ -90,25 +70,38 @@ export default class Location extends React.Component {
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           }}>
-            {this.state.location && (
-              <View>
-            <Marker
-              coordinate= {coord}
-              title= "Utilisateur"
-              description="position actuelle"
-            />
-            <Marker
-              coordinate= {coord2}
-              title= "Utilisateur5"
-              description="position actuelle"
-            />
+
+          {this.state.location && (
+            <View>
+              <Marker
+                coordinate={coord}
+                title="Utilisateur"
+                description="position actuelle"
+              />
             </View>
-            )}
+          )}
+          {this.state.all && this.state.all.map(marker => (
+            <View key={marker.fields.station_name}>
+              <Marker
+                coordinate={{
+                  latitude: marker.fields.geo[0],
+                  longitude: marker.fields.geo[1]
+                }}
+                title={marker.fields.station_name}
+                description={marker.fields.station_name}
+              />
+            </View>
+          ))}
         </MapView>
 
-        <Text>
-          {lat} - {long}
+        {!this.state.all &&
+          <View>
+            <ActivityIndicator size="large" color="#0000ff" />
+            <Text style={{ fontWeight: 'bold', fontSize: 20 }}>
+              Localisation en cours
         </Text>
+          </View>
+        }
       </View>
     );
   }
